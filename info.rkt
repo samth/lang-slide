9736a35f8900dba974ab4fc7383d160483425108
Georges Dupéron <georges.duperon@gmail.com>
2016-09-22 15:04:52 +0200

    Fixed missing dependency

--- info.rkt
+++ info.rkt
@@ -1,4 +1,5 @@
 #lang setup/infotab
 
 (define collection 'multi)
+(define build-deps '("scribble-lib" "racket-doc" "pict-doc"))
 (define deps '("base" "draw-lib" "gui-lib" "scheme-lib" "slideshow-lib" "unstable-lib"))
