cda7465ad42514293783f96a1e3cd0171bbbf77a
Georges Dupéron <georges.duperon@gmail.com>
2016-09-22 16:25:09 +0200

    Merge the build-dep and dep of the two info.rkt

--- info.rkt
+++ info.rkt
@@ -3,3 +3,5 @@
 (define collection 'multi)
 (define build-deps '("scribble-lib" "racket-doc" "pict-doc"))
 (define deps '("base" "draw-lib" "gui-lib" "scheme-lib" "slideshow-lib" "unstable-lib"))
+(define pkg-desc "A picture showing all the languages used to implement Racket.")
+(define version "1.0")
